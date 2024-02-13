9264d26fb280129b816f61d3e7647f3d85ec6e88
marco-soria <marco.soria@pucp.edu.pe>
2024-02-13 17:00:08 -0500

    searchAnime completed

--- src/pages/anime/RankingAnime.jsx
+++ src/pages/anime/RankingAnime.jsx
@@ -1,94 +1,106 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import './RankingAnime.css';
+import './SearchAnime.css';
 
 const RankingAnime = () => {
-    const [animeList, setAnimeList] = useState([]);
-    const [selectedSynopsis, setSelectedSynopsis] = useState('');
-    const [selectedTitle, setSelectedTitle] = useState('');
+  const [animeList, setAnimeList] = useState([]);
+  const [selectedSynopsis, setSelectedSynopsis] = useState('');
+  const [selectedTitle, setSelectedTitle] = useState('');
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-    const handleSynopsisClick = (synopsis, title) => {
-        const cleanedSynopsis = synopsis.replace(/\\"/g, '"').replace(/\\n/g, '<br>');
-        setSelectedSynopsis(cleanedSynopsis);
-        setSelectedTitle(title);
+  const handleSynopsisClick = (synopsis, title) => {
+    const cleanedSynopsis = JSON.stringify(synopsis).replace(/\\"/g, '"').replace(/\\n/g, '<br>');
+    setSelectedSynopsis(cleanedSynopsis);
+    setSelectedTitle(title);
+    setIsModalOpen(true);
+  };
+
+  useEffect(() => {
+    const url = "https://api.jikan.moe/v4/top/anime";
+    
+    const obtenerAnime = async () => {
+      try {
+        const response = await fetch(`${url}?limit=12`);
+        const data = await response.json();
+        setAnimeList(data.data);
+      } catch (error) {
+        console.error('Error al obtener anime:', error);
+      }
     };
+    
+    obtenerAnime();
+  }, []);
 
-    useEffect(() => {
-        const obtenerAnime = async () => {
-            const url = "https://api.jikan.moe/v4/top/anime?limit=12";
-            try {
-                const response = await fetch(url);
-                const data = await response.json();
-                setAnimeList(data.data);
-            } catch (error) {
-                console.error('Error fetching anime:', error);
-            }
-        };
-
-        obtenerAnime();
-    }, []);
-
-    return (
-        <section className="container-fluid mb-4">
-            <div className="row " id="row-ranking">
-                <div className="col-md-12 d-flex align-items-center" style={{border: 'solid 3px'}}>
-                    <i className="fa-sharp fa-solid fa-star fa-xl mb-2" style={{color: '#C21292'}}></i>
-                    <h2 className="fs-2">&#160;Ranking&#160;</h2>
-                    <i className="fa-sharp fa-solid fa-star fa-xl mb-2" style={{color: '#C21292'}}></i>
+  return (
+    <section className="container-fluid mb-4">
+      <div className="row" id="row-ranking">
+        {animeList.map((result, index) => {
+          return (
+            <div key={index} className="col-md-3 mt-3 g-2">
+              <div className="card formbg">
+                <img
+                  className="card border-0"
+                  style={{ maxHeight: '40vh', objectFit: 'cover' }}
+                  src={result.images.jpg.large_image_url}
+                  alt={result.title}
+                />
+                <div className="card-body text-center">
+                  <h6 className="text-title fs-4" style={{ color: '#9500ff' }}>N. {result.rank}</h6>
+                  <h4 className="text-title" style={{ color: '#9500ff', overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}>{result.title}</h4>
+                  <p className="text-title fs-5">Score: {result.score}</p>
+                  <button
+                    type="button"
+                    className="btn btn-secondary signupbutton"
+                    onClick={() => handleSynopsisClick(result.synopsis, result.title)}
+                  >
+                    Synopsis
+                  </button>
                 </div>
+              </div>
             </div>
+          );
+        })}
+      </div>
 
-            <div className="row">
-                {animeList.map((anime, index) => (
-                    <div className="col-md-3 mt-3 g-2" key={index}>
-                        <div className="card formbg">
-                            <img
-                                className="card-img-top"
-                                style={{maxHeight: '40vh', objectFit: 'cover'}}
-                                src={anime.images.jpg.large_image_url}
-                                alt={anime.title}
-                            />
-                            <div className="card-body text-center">
-                                <h6 className="text-title fs-4" style={{color: '#9500ff'}}>N. {anime.rank}</h6>
-                                <h4 className="text-title" style={{color: '#9500ff', overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap'}}>{anime.title}</h4>
-                                <p className="text-title fs-5">Score: {anime.score}</p>
-                                <button
-                                    type="button"
-                                    className="btn btn-primary signupbutton"
-                                    data-bs-toggle="modal"
-                                    data-bs-target="#synopsisModal"
-                                    onClick={() => handleSynopsisClick(anime.synopsis, anime.title)}
-                                >
-                                    Synopsis
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                ))}
+      {isModalOpen && (
+        <div className="modal fade show" id="synopsisModal" tabIndex="-1" aria-labelledby="synopsisModalLabel" aria-hidden="true" style={{ display: 'block' }}>
+          <div className="modal-dialog modal-dialog-centered modal-xl">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="synopsisModalLabel">{selectedTitle}</h5>
+                <button type="button" className="btn-close" onClick={() => setIsModalOpen(false)} aria-label="Close"></button>
+              </div>
+              <div className="modal-body">
+                {/* Mostrar la sinopsis formateada */}
+                <p dangerouslySetInnerHTML={{ __html: selectedSynopsis }}></p>
+              </div>
+              <div className="modal-footer">
+                <button type="button" className="btn btn-secondary" onClick={() => setIsModalOpen(false)}>Close</button>
+              </div>
             </div>
-
-            <div className="modal fade" id="synopsisModal" tabIndex="-1" aria-labelledby="synopsisModalLabel" aria-hidden="true">
-                <div className="modal-dialog modal-dialog-centered modal-xl">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <h5 className="modal-title" id="synopsisModalLabel">{selectedTitle}</h5>
-                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div className="modal-body" id="synopsisContent">
-                        <p dangerouslySetInnerHTML={{ __html: selectedSynopsis }}></p>
-                        </div>
-                        <div className="modal-footer">
-                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-    );
+          </div>
+        </div>
+      )}
+    </section>
+  );
 };
 
 export { RankingAnime };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
