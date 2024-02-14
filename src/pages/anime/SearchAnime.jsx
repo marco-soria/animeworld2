e09d7fdfca03cdaa4a5c57b3f9c4c213c1f7267e
marco-soria <marco.soria@pucp.edu.pe>
2024-02-14 13:01:56 -0500

    modal close button

--- src/pages/anime/SearchAnime.jsx
+++ src/pages/anime/SearchAnime.jsx
@@ -122,7 +122,7 @@ const SearchAnime = () => {
                     <div className="modal-content">
                         <div className="modal-header">
                             <h5 className="modal-title" id="synopsisModalLabel">{selectedTitle}</h5>
-                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" ></button>
                         </div>
                         <div className="modal-body">
                             <p dangerouslySetInnerHTML={{ __html: selectedSynopsis }}></p>
